8f53eede5f1f27d5f9356d848133740049e35569
spkyu <tou2913@gmail.com>
2021-04-20 10:44:04 +0900

    놀이공원 상수수정

--- Desktop/git/Amunsement/src/Ticketing/Cacuage.java
+++ Desktop/git/Amunsement/src/Ticketing/Cacuage.java
@@ -6,39 +6,39 @@ public class Cacuage {
 		int ticketprice = 0;
 		if (daynight == 1) {
 			if ((realage <= 21) && (realage >= 20)) {
-				ticketprice = 0;
+				ticketprice = Numbers.ticket_Price_Day_new;// 애
 				return ticketprice;
 			} else if ((realage <= 19) && (realage >= 10)) {
-				ticketprice = 44000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Day_child * amountticket;// 초등학생
 				return ticketprice;
 			} else if ((realage <= 9) && (realage >= 4)) {
-				ticketprice = 47000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Day_teenager * amountticket;// 청소년
 				return ticketprice;
 			} else if (((realage <= 3) && (realage >= 0)) || ((realage <= 99) && (realage >= 58))) {
-				ticketprice = 56000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Day_adult * amountticket;// 어른
 				return ticketprice;
 			} else if ((realage <= 57) && (realage > 22)) {
-				ticketprice = 44000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Day_senior * amountticket;// 노인
 				return ticketprice;
 			} else {
 				return ticketprice;
 			}
-		
-		}else if(daynight==2) {
+
+		} else if (daynight == 2) {
 			if ((realage <= 21) && (realage >= 20)) {
-				ticketprice = 0;
+				ticketprice = Numbers.ticket_Price_Night_new;
 				return ticketprice;
 			} else if ((realage <= 19) && (realage >= 10)) {
-				ticketprice = 37000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Night_child * amountticket;
 				return ticketprice;
 			} else if ((realage <= 9) && (realage >= 4)) {
-				ticketprice = 40000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Night_teenager * amountticket;
 				return ticketprice;
 			} else if (((realage <= 3) && (realage >= 0)) || ((realage <= 99) && (realage >= 58))) {
-				ticketprice = 46000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Night_adult * amountticket;
 				return ticketprice;
 			} else if ((realage <= 57) && (realage > 22)) {
-				ticketprice = 37000 * amountticket;
+				ticketprice = Numbers.ticket_Price_Night_senior * amountticket;
 				return ticketprice;
 			} else {
 				return ticketprice;
@@ -74,19 +74,19 @@ public class Cacuage {
 	public int discount(int discount, int ticketprice) {
 
 		if (discount == 1) {
-			ticketprice = ticketprice;// 없음
+			ticketprice = ticketprice * Numbers.Discount_no;// 없음
 			return ticketprice;
 		} else if (discount == 2) {// 장애인
-			ticketprice = ticketprice * 60 / 100;
+			ticketprice = ticketprice * Numbers.Discount_disable;
 			return ticketprice;
 		} else if (discount == 3) {// 국가유공자
-			ticketprice = ticketprice * 50 / 100;
+			ticketprice = ticketprice * Numbers.Discount_nationmerit;
 			return ticketprice;
 		} else if (discount == 4) {// 다자녀
-			ticketprice = ticketprice * 80 / 100;
+			ticketprice = ticketprice * Numbers.Discount_manychild;
 			return ticketprice;
 		} else if (discount == 5) {// 임산부
-			ticketprice = ticketprice * 75 / 100;
+			ticketprice = ticketprice * Numbers.Discount_pregnant;
 			return ticketprice;
 		} else {
 			return ticketprice;
@@ -116,19 +116,19 @@ public class Cacuage {
 		}
 
 	}
-	
+
 	public String strdaynight(int daynight) {
-		String str="";
-		if(daynight==1) {
-			str="주간";
+		String str = "";
+		if (daynight == 1) {
+			str = "주간";
 			return str;
-		}else if(daynight==2) {
-			str="야간";
+		} else if (daynight == 2) {
+			str = "야간";
 			return str;
-		}else {
+		} else {
 			return str;
-			
+
 		}
 	}
-	
+
 }
--- Desktop/git/Amunsement/src/Ticketing/Numbers.java
+++ Desktop/git/Amunsement/src/Ticketing/Numbers.java
@@ -5,7 +5,7 @@ public class Numbers {
 
 	public static int count = 0;
 	public static int totalsum = 0;
-	
+
 	public static String strdaynight = "";
 	public static String strold = "";
 	public static String strdiscount = "";
@@ -18,6 +18,23 @@ public class Numbers {
 	public static int countinue = 0;
 	public static int countinue2 = 0;
 	public static int realage = 0;
-	
+
 	public static String Filewriteadress = "test.csv";
+
+	public static int ticket_Price_Day_new = 0;
+	public static int ticket_Price_Day_adult = 56000;
+	public static int ticket_Price_Day_child = 44000;
+	public static int ticket_Price_Day_senior = 44000;
+	public static int ticket_Price_Day_teenager = 47000;
+	public static int ticket_Price_Night_new = 0;
+	public static int ticket_Price_Night_adult = 46000;
+	public static int ticket_Price_Night_child = 37000;
+	public static int ticket_Price_Night_senior = 37000;
+	public static int ticket_Price_Night_teenager = 40000;
+
+	public static int Discount_no=1;
+	public static int Discount_disable=60/100;
+	public static int Discount_nationmerit=50/100;
+	public static int Discount_manychild=80/100;
+	public static int Discount_pregnant=75/100;
 }

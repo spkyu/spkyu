ca0a2739b4d1b155a7beea77c2383c95b87285dc
spkyu <tou2913@gmail.com>
2021-04-20 11:31:29 +0900

    0420매출분석 수정 갯수까지

--- Desktop/git/Ticketsales/src/Salesticket/calcu.java
+++ Desktop/git/Ticketsales/src/Salesticket/calcu.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import Ticketing.Filewrite;
-
 public class calcu {
 
 	public void calday() throws IOException {
@@ -36,7 +34,7 @@ public class calcu {
 				// System.out.print(arr[z]);
 
 			}
-			
+
 			row++;
 
 		}
@@ -48,23 +46,65 @@ public class calcu {
 				sumday += Integer.parseInt(indat[i][4]);
 				countday += Integer.parseInt(indat[i][3]);
 				today = indat[i][0];
-				strday = "주"+","+today+","+sumday+","+countday;
 
 			} else if (indat[i][0].equals(date) && indat[i][1].equals("야간")) {
 				sumnight += Integer.parseInt(indat[i][4]);
 				countnight += Integer.parseInt(indat[i][3]);
 				today = indat[i][0];
-				strnight = "야"+","+today+","+sumnight+","+countnight;
 
 			}
 
 		}
+		int amounttket_day_new = 0;
+		int amounttket_day_child = 0;
+		int amounttket_day_teenager = 0;
+		int amounttket_day_adult = 0;
+		int amounttket_day_senior = 0;
+		for (int i = 0; i < row - 1; i++) {
+			if (indat[i][1].equals("주간") && indat[i][2].equals("유아")) {
+				amounttket_day_new += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("주간") && indat[i][2].equals("청소년")) {
+				amounttket_day_teenager += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("주간") && indat[i][2].equals("소아")) {
+				amounttket_day_child += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("주간") && indat[i][2].equals("대인")) {
+				amounttket_day_adult += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("주간") && indat[i][2].equals("노인")) {
+				amounttket_day_senior += Integer.parseInt(indat[i][3]);
+			}
+		}
+		int amounttket_night_new = 0;
+		int amounttket_night_child = 0;
+		int amounttket_night_teenager = 0;
+		int amounttket_night_adult = 0;
+		int amounttket_night_senior = 0;
+		for (int i = 0; i < row - 1; i++) {
+			if (indat[i][1].equals("야간") && indat[i][2].equals("유아")) {
+				amounttket_night_new += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("야간") && indat[i][2].equals("청소년")) {
+				amounttket_night_teenager += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("야간") && indat[i][2].equals("소아")) {
+				amounttket_night_child += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("야간") && indat[i][2].equals("대인")) {
+				amounttket_night_adult += Integer.parseInt(indat[i][3]);
+			} else if (indat[i][1].equals("야간") && indat[i][2].equals("노인")) {
+				amounttket_night_senior += Integer.parseInt(indat[i][3]);
+			}
+		}
+
+		strday = today + "," + "주" + "," + sumday + "," + countday + "(" + amounttket_day_new + ","
+				+ amounttket_day_child + "," + amounttket_day_teenager + "," + amounttket_day_adult + "," + ""
+				+ amounttket_day_senior + ")";
+		strnight = today + "," + "야" + "," + sumnight + "," + countnight + "(" + amounttket_night_new + ","
+				+ amounttket_night_child + "," + amounttket_night_teenager + "," + amounttket_night_adult + ","
+				+ amounttket_night_senior + ")";
+
 		fileWrite fwfw = new fileWrite();
-		String head= "주야구분"+"날짜"+","+"판매금액"+","+"매수";
+		String head = "날짜/" + "주야구분/" + "판매금액/" + "매수(유아 소아 청소년 대인 노인)";
 		System.out.println(head);
 		System.out.println(strday);
 		System.out.println(strnight);
-		fwfw.filewrite(head+"\n"+strday+"\n"+strnight+"\n");
+		fwfw.filewrite(head + "\n" + strday + "\n" + strnight + "\n");
 	}
 
 }
